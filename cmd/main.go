9e85d13c5db1aa947951741470001286a61bab5d
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2025-06-30 20:53:21 -0300

    refact main.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,17 +9,13 @@ import (
 
 func main() {
 	ready := make(chan struct{})
-
 	go func() {
 		if err := server.InitServer(ready); err != nil {
 			log.Fatalf("Erro ao iniciar servidor: %v", err)
 		}
 	}()
-
-	// Wait until server is ready
 	<-ready
 
-	// Now call client
 	if err := client.FetchExchangeRate(); err != nil {
 		log.Fatalf("Erro ao buscar taxa de câmbio: %v", err)
 	}
